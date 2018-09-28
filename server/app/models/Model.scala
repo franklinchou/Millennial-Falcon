8fa82528b7ed99c75fb5d04b3aabaa58d6c93ba4
Franklin Chou <franklin@peeriq.com>
2018-09-28 14:20:46 -0400

    create implicit converstion vertex => model

--- server/app/models/Model.scala
+++ server/app/models/Model.scala
@@ -4,7 +4,6 @@ import java.time.{ZoneOffset, ZonedDateTime}
 import java.util.UUID
 
 import lib.StringContainer
-import models.Model.DefaultTime
 import models.fields.ModelId
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource
 import org.apache.tinkerpop.gremlin.structure.Vertex
@@ -17,7 +16,7 @@ object Model {
   val Name = "name"
   val Type = "model-type"
   val CreatedAt = "created-at"
-  val UpdatedAt = "updated-at"
+  val ModifiedAt = "modified-at"
 
   val UserType = "user"
   val GroupType = "group"
@@ -31,11 +30,11 @@ object Model {
   def add[T <: AnyVal](m: Model[T], jg: GraphTraversalSource): Vertex = {
     jg
       .addV(m.`type`)
+      .property(Model.Type, m.`type`)
       .property(Model.Name, m.name.value)
       .property(Model.Id, m.id.value)
-      .property(Model.Type, m.`type`)
       .property(Model.CreatedAt, m.createdAt.toString)
-      .property(Model.UpdatedAt, m.updatedAt.toString)
+      .property(Model.ModifiedAt, m.modifiedAt.toString)
       .next()
   }
 
@@ -47,15 +46,15 @@ object Model {
   */
 trait Model[T <: AnyVal] {
 
-  val id: StringContainer[ModelId] = Model.generateUUID[ModelId]
+  val id: StringContainer[ModelId]
 
   val name: StringContainer[T]
 
   val `type`: String
 
-  val createdAt: ZonedDateTime = DefaultTime
+  val createdAt: ZonedDateTime
 
-  val updatedAt: ZonedDateTime = DefaultTime
+  val modifiedAt: ZonedDateTime
 
   lazy val modelType: String = getClass.getSimpleName
 
--- server/app/models/UserModel.scala
+++ server/app/models/UserModel.scala
@@ -1,8 +1,10 @@
 package models
 
+import java.time.ZonedDateTime
+
 import ai.x.play.json.Jsonx
 import lib.StringContainer
-import models.fields.UserName
+import models.fields.{ModelId, UserName}
 import org.apache.tinkerpop.gremlin.structure.Vertex
 
 
@@ -10,25 +12,65 @@ object UserModel {
 
   implicit lazy val jsFormat = Jsonx.formatCaseClass[UserModel]
 
+  /**
+    * Convert a string to zoned date time
+    *
+    * @param s
+    * @return
+    */
+  implicit def string2ZonedDatetime(s: String): ZonedDateTime = {
+    ZonedDateTime.parse(s)
+  }
+
+  /**
+    * Convert a user vertex to a [[UserModel]]
+    *
+    * @param v
+    * @return
+    */
   implicit def vertex2Model(v: Vertex): Option[UserModel] = {
 
     val test = ModelUtils.testVertex(v, Model.UserType)
 
     if (test) {
+      val id = v.property(Model.Id).toString
       val name = v.property(Model.Name).toString
-      val model = UserModel.apply(StringContainer.apply[UserName](name))
+      val createdAt: ZonedDateTime = v.property(Model.CreatedAt).toString
+      val modifiedAt: ZonedDateTime = v.property(Model.ModifiedAt).toString
+
+      val model =
+        UserModel
+          .apply(
+            id = StringContainer.apply[ModelId](id),
+            name = StringContainer.apply[UserName](name),
+            createdAt = createdAt,
+            modifiedAt = modifiedAt
+          )
+
       Some(model)
     } else {
       None
     }
-
   }
 
 
 }
 
-case class UserModel(name: StringContainer[UserName]) extends Model[UserName] {
+case class UserModel(id: StringContainer[ModelId],
+                     name: StringContainer[UserName],
+                     createdAt: ZonedDateTime,
+                     modifiedAt: ZonedDateTime) extends Model[UserName] {
 
   val `type`: String = Model.UserType
+
+  def apply(name: StringContainer[UserName]): UserModel = {
+    UserModel(
+      id = Model.generateUUID[ModelId],
+      name = name,
+      createdAt = Model.DefaultTime,
+      modifiedAt = Model.DefaultTime
+    )
+  }
+
 
 }
