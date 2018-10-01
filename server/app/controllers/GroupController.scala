193a7f88d7b65d5c9939e366130eb39fe97392b8
Franklin Chou <franklin@peeriq.com>
2018-09-30 20:02:37 -0400

    group controller

--- server/app/controllers/GroupController.scala
+++ server/app/controllers/GroupController.scala
@@ -1,13 +1,24 @@
 package controllers
 
 import javax.inject._
+import play.api.libs.json.Json
 import play.api.mvc._
+import services.GroupService
+
+import scala.concurrent.ExecutionContext
 
 @Singleton
-class GroupController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class GroupController @Inject()(cc: ControllerComponents,
+                                groupService: GroupService)
+                               (implicit ec: ExecutionContext) extends AbstractController(cc) {
 
-  def index() = Action { implicit rq: Request[AnyContent] =>
-    Ok("")
+  def index() = Action.async { implicit rq: Request[AnyContent] =>
+    groupService
+      .findAllGroups
+      .map { models =>
+        val json = Json.toJson(models)
+        Ok(json)
+      }
   }
 
 }
