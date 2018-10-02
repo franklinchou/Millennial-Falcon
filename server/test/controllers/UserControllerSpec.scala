2dfa3af1b51f00cfd848b728809056823c7d149b
Franklin Chou <franklin@peeriq.com>
2018-10-02 15:20:49 -0400

    test

--- server/test/controllers/UserControllerSpec.scala
+++ server/test/controllers/UserControllerSpec.scala
@@ -1,23 +1,24 @@
 package controllers
 
+import akka.util.Timeout
 import lib.StringContainer
 import models.vertex.UserModel
 import org.mockito.Mockito.when
 import org.scalatest.mockito.MockitoSugar
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.test.Helpers.{GET, OK, status, stubControllerComponents}
 import play.api.test.{FakeRequest, Injecting}
 import services.UserService
 
-import play.api.test.Helpers.{GET, OK, status, stubControllerComponents}
-
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
 class UserControllerSpec extends PlaySpec with MockitoSugar with GuiceOneAppPerTest with Injecting {
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  "User Controller should" {
+  "User Controller" should {
 
     val mockUserService = mock[UserService]
 
@@ -29,15 +30,15 @@ class UserControllerSpec extends PlaySpec with MockitoSugar with GuiceOneAppPerT
         UserModel.apply(StringContainer.apply("mock-2"))
       )
 
-
     "show all users" in {
       when(mockUserService.findAllUsers).thenReturn(Future { mockResults })
       val request = FakeRequest(GET, s"/users")
       val method = controller.index()(request)
-      status(method) mustBe OK
+
+      // TODO check status for now, how to check return value?
+      status(method)(Timeout(5.seconds)) mustBe OK
     }
 
-    0
   }
 
 
