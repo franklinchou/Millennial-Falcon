c8fbb36a9cbabb72a881f4cc2e05d10fc4ba8f6d
Franklin Chou <franklin@peeriq.com>
2018-10-02 15:05:33 -0400

    saving

--- server/test/controllers/UserControllerSpec.scala
+++ server/test/controllers/UserControllerSpec.scala
@@ -1,5 +1,44 @@
 package controllers
 
-class UserControllerSpec {
+import lib.StringContainer
+import models.vertex.UserModel
+import org.mockito.Mockito.when
+import org.scalatest.mockito.MockitoSugar
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.test.{FakeRequest, Injecting}
+import services.UserService
+
+import play.api.test.Helpers.{GET, OK, status, stubControllerComponents}
+
+import scala.concurrent.Future
+
+class UserControllerSpec extends PlaySpec with MockitoSugar with GuiceOneAppPerTest with Injecting {
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  "User Controller should" {
+
+    val mockUserService = mock[UserService]
+
+    val controller = new UserController(stubControllerComponents(), mockUserService)
+
+    val mockResults =
+      List(
+        UserModel.apply(StringContainer.apply("mock-1")),
+        UserModel.apply(StringContainer.apply("mock-2"))
+      )
+
+
+    "show all users" in {
+      when(mockUserService.findAllUsers).thenReturn(Future { mockResults })
+      val request = FakeRequest(GET, s"/users")
+      val method = controller.index()(request)
+      status(method) mustBe OK
+    }
+
+    0
+  }
+
 
 }
