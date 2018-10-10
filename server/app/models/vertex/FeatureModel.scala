f88f82e771ccd00bd4140a1e4297dfb28a5bd93e
Franklin Chou <franklin@peeriq.com>
2018-10-10 11:15:27 -0400

    feature model

--- server/app/models/vertex/FeatureModel.scala
+++ server/app/models/vertex/FeatureModel.scala
@@ -6,6 +6,45 @@ import lib.StringContainer
 import models.field.{FeatureField, IdField}
 import models.vertex
 import org.apache.tinkerpop.gremlin.structure.Vertex
+import play.api.libs.json.{Json, OWrites}
+
+object FeatureModel {
+
+  implicit lazy val jsWriter: OWrites[FeatureModel] = (fm: FeatureModel) => {
+    Json.obj(
+      "type" -> fm.`type`,
+      "id" -> fm.id.value,
+      "attributes" -> Json.obj(
+        "feature" -> fm.name,
+        "created-at" -> fm.createdAt.toString,
+        "modified-at" -> fm.modifiedAt.toString
+      )
+    )
+  }
+
+  /**
+    * Marshall a feature vertex to [[FeatureModel]]
+    * 
+    * @param v
+    * @return
+    */
+  implicit def vertex2FeatureModel(v: Vertex): FeatureModel = {
+    val id = v.property(vertex.Id).value.toString
+    val feature = v.property(vertex.Name).value.toString
+    val createdAt = v.property(vertex.CreatedAt).value.toString
+    val modifiedAt = v.property(vertex.ModifiedAt).value.toString
+
+    FeatureModel
+      .apply(
+        id = StringContainer.apply[IdField](id),
+        name = StringContainer.apply[FeatureField](feature),
+        createdAt = ZonedDateTime.parse(createdAt),
+        modifiedAt = ZonedDateTime.parse(modifiedAt)
+      )
+  }
+
+}
+
 
 case class FeatureModel(id: StringContainer[IdField],
                         name: StringContainer[FeatureField],
@@ -13,7 +52,5 @@ case class FeatureModel(id: StringContainer[IdField],
                         modifiedAt: ZonedDateTime) extends Model[FeatureField] {
 
   val `type` = vertex.FeatureType
-
-  def add(m: Model[FeatureField]): Vertex = ???
 
 }
